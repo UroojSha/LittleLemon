9a08bb754cf75b70f9c980ae9432b7cc3dead133
UroojSha <urooj_shafait292@hotmail.com>
2024-02-05 21:10:17 +0000

    Your commit message here

---
+++ src/components/Main/Highlight Section/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Card.css";
+
+function Card() {
+  return (
+    <article className="card-container">
+      <div className="card-image"></div>
+      <h3 className="card-title">Lemon Meringue Pie</h3>
+      <div className="card-price">$4.99</div>
+      <p className="card-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare
+        lectus elit, a luctus sem mollis sit amet.
+      </p>
+      <div className="card-link">Order Delivery</div>
+    </article>
+  );
+}
+
+export default Card;
